aa573a90aca2fe4439e5d0bbb8f092deb665f793
sunhao <332449142@qq.com>
2017-09-13 15:54:18 +0800

    我又新加入了一个功能

--- first.java
+++ first.java
@@ -3,6 +3,7 @@ class Untitled {
 		System.out.println("I am testing git version control system.");
 		System.out.println("Modified.");
 		System.out.println("nihao");
+		System.out.println("hello nihao");
 	}
 }
 
