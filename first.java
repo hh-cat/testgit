6b066dd2e20463df6206b880b44bc9e5ee3def9e
sunhao <332449142@qq.com>
2017-09-13 15:52:34 +0800

    wrong two

--- first.java
+++ first.java
@@ -2,6 +2,7 @@ class Untitled {
 	public static void main(String[] args) {
 		System.out.println("I am testing git version control system.");
 		System.out.println("Modified.");
+		System.out.println("nihao");
 	}
 }
 
